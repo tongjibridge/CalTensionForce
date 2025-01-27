48edc239ade083a3a634612788b587c8e00e548f
tongjibridge <52105283+tongjibridge@users.noreply.github.com>
2025-01-28 02:33:56 +0800

    更新api.py，自动获取最后的stage和step
    MIDAS获取索力结果表格的输入里"STAGE_STEP"为stage_name:step_name，本次更新将step_name改为自动获取

--- api.py
+++ api.py
@@ -1,9 +1,14 @@
 # ruff: noqa: F401
 import requests
 import json
-from tools import *
+from tools import (
+    MidasConfig,
+    Pretension_Loads_df_to_json,
+    truss_force_tablejson_to_table,
+    Pretension_Loads_json_to_df,
+)
 import os
-
+import re
 
 # 创建MidasConfig实例
 midas_config = MidasConfig()
@@ -45,12 +50,13 @@ def delete_iteration_files():
             print(f"Deleted file: {filename}")
 
 
-def compute_tension(tension, target):
+def compute_tension(tension: str, target: str, eps: float = 0.15):
     """
     计算并调整索力，直到偏差百分比满足要求。
 
     :param tension: 包含索力数据的JSON文件路径。
     :param target: 目标索力数据的JSON文件路径。
+    :param eps: 允许的最大偏差百分比，默认为0.15。
     """
     # 删除运行目录里名称为迭代+数字的xlsx文件
     delete_iteration_files()
@@ -64,21 +70,15 @@ def compute_tension(tension, target):
     eles = target_tension["单元号"].astype(int)
     eles = eles.tolist()
 
-    # 定义POST请求的JSON数据
-    POST_json = {
-        "Argument": {
-            "TABLE_NAME": "TrussForce",
-            "TABLE_TYPE": "TRUSSFORCE",
-            "EXPORT_PATH": "G:\\cae\\wgfl\\api\\Output.json",
-            "UNIT": {"FORCE": "N", "DIST": "m"},
-            "STYLES": {"FORMAT": "Fixed", "PLACE": 12},
-            "COMPONENTS": ["Elem", "Load", "Stage", "Step", "Force-I", "Force-J"],
-            "NODE_ELEMS": {"KEYS": eles},
-            "LOAD_CASE_NAMES": ["合计(CS)"],
-            "OPT_CS": True,
-            "STAGE_STEP": ["成桥状态:002(最后)"],
-        }
-    }
+    # 获取当前文件夹的路径
+    current_folder = os.getcwd()
+
+    # 获取所有阶段的信息
+    allstage = MidasAPI("GET", "/db/STAG", {})
+    # 获取最后一个阶段的信息
+    last_step_information = list(allstage["STAG"].items())[-1]
+    # 获取最后一个阶段的名称
+    stagename = last_step_information[-1]["NAME"]
 
     # 读取索力JSON文件
     with open(tension, "r", encoding="utf-8") as f:
@@ -97,17 +97,84 @@ def compute_tension(tension, target):
         MidasAPI("PUT", "/db/PTNS", tension_json)
         # 发送POST请求进行分析
         MidasAPI("POST", "/doc/Anal", {})
+        if n == 1:
+            # 获取最后的step
+            POST_json = {
+                "Argument": {
+                    "TABLE_NAME": "TrussForce",
+                    "TABLE_TYPE": "TRUSSFORCE",
+                    "EXPORT_PATH": "E:\\CAE\\model\\mgfl\\api\\Output2.json",
+                    "UNIT": {"FORCE": "kN", "DIST": "m"},
+                    "STYLES": {"FORMAT": "Fixed", "PLACE": 12},
+                    "COMPONENTS": [
+                        "Elem",
+                        "Load",
+                        "Stage",
+                        "Step",
+                        "Force-I",
+                        "Force-J",
+                    ],
+                    "NODE_ELEMS": {"KEYS": [eles[0]]},
+                    "LOAD_CASE_NAMES": ["合计(CS)"],
+                    "OPT_CS": True,
+                }
+            }
+            MidasAPI("POST", "/POST/TABLE", POST_json)
+            with open(
+                "Output2.json",
+                "r",
+                encoding="gbk",
+                errors="ignore",
+            ) as f:
+                temp_json2 = f.read()
+            extracted_string = re.search(r"(\"TrussForce\":+)(.+)}", temp_json2).group(
+                0
+            )
+            temp_json2 = json.loads("{" + extracted_string)
+            temp_value2 = truss_force_tablejson_to_table(temp_json2)
+            step_name = temp_value2["Step"].iloc[-3]
+
+            # 格式化阶段步骤名称
+            STAGE_STEP = f"{stagename}:{step_name}"
+
+            # 定义POST请求的JSON数据
+            POST_json = {
+                "Argument": {
+                    "TABLE_NAME": "TrussForce",
+                    "TABLE_TYPE": "TRUSSFORCE",
+                    "EXPORT_PATH": f"{current_folder}\\Output.json",
+                    "UNIT": {"FORCE": "N", "DIST": "m"},
+                    "STYLES": {"FORMAT": "Fixed", "PLACE": 12},
+                    "COMPONENTS": [
+                        "Elem",
+                        "Load",
+                        "Stage",
+                        "Step",
+                        "Force-I",
+                        "Force-J",
+                    ],
+                    "NODE_ELEMS": {"KEYS": eles},
+                    "LOAD_CASE_NAMES": ["合计(CS)"],
+                    "OPT_CS": True,
+                    "STAGE_STEP": [STAGE_STEP],
+                }
+            }
+
         # 发送POST请求导出表格数据
         MidasAPI("POST", "/POST/TABLE", POST_json)
 
         # 读取导出的JSON文件
         with open(
-            "G:/cae/wgfl/api/Output.JSON", "r", encoding="utf-8-sig", errors="replace"
+            f"{current_folder}\\Output.json",
+            "r",
+            encoding="utf-8-sig",
+            errors="replace",
         ) as f:
             temp_json = json.load(f)
 
         # 将导出的JSON文件转换为表格数据
         temp_value = truss_force_tablejson_to_table(temp_json)
+
         # 将索力JSON文件转换为DataFrame
         tension_value = Pretension_Loads_json_to_df(tension_json)
 
@@ -127,7 +194,7 @@ def compute_tension(tension, target):
         target_tension.to_excel(f"迭代{n:02d}.xlsx", index=False)
 
         # 如果偏差百分比的绝对值均小于0.15%，结束循环
-        if abs(target_tension["偏差百分比"]).max() < 0.15:
+        if abs(target_tension["偏差百分比"]).max() < eps:
             break
 
         # 更新索力JSON文件中的张力数据
@@ -135,9 +202,10 @@ def compute_tension(tension, target):
         tension_json = Pretension_Loads_df_to_json(tension_value)
     return target_tension
 
-if __name__ == "__main__":
-    compute_tension("target.json", "tension.json")
 
+if __name__ == "__main__":
+    # compute_tension("target.json", "tension.json")
+    # 测试代码1——获取初拉力
     # # ljson = MidasAPI("GET", "/db/PTNS")
 
     # # # save json
@@ -145,6 +213,8 @@ if __name__ == "__main__":
     # #     f.write(json.dumps(ljson))
 
     # # elementIDS=["2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021","2022","2023","2024","2025","2026","2027","2028","2029","2030","2031","2032","2033","2034","2035","2036","2037","2038","2039","2040","2041","2042","2043","2044","2045","2046","2047","2048","2049","2050","2051","2052","2053","2054","2055","2056","2057","2058","2059","2060","2061","2062","2063","2064","2065","2066","2067","2068","2069","2070","2071","2072"]
+
+    # 测试代码2——修改初拉力并计算
     # # with open('./accounts.json', 'r') as f:
     # #     data = json.load(f)
     # # data['PTNS']['2001']['ITEMS'][0]["TENSION"] = 200000
@@ -152,18 +222,40 @@ if __name__ == "__main__":
     # # MidasAPI("PUT", "/db/PTNS", tension_json)
     # # response = MidasAPI("POST", "/doc/Anal", {})
 
+    # 测试代码3——获取索力结果表格
+    # eles = [2005, 2006, 2007]
     # POST_json = {
     #     "Argument": {
     #         "TABLE_NAME": "TrussForce",
     #         "TABLE_TYPE": "TRUSSFORCE",
-    #         "EXPORT_PATH": "G:\\cae\\wgfl\\api\\Output.json",
+    #         "EXPORT_PATH": "E:\\CAE\\model\\mgfl\\api\\Output2.json",
     #         "UNIT": {"FORCE": "kN", "DIST": "m"},
     #         "STYLES": {"FORMAT": "Fixed", "PLACE": 12},
-    #         "COMPONENTS": ["Elem", "Load", "Stage", "Step","Force-I", "Force-J"],
-    #         "NODE_ELEMS": {"KEYS": [2005,2006]},
+    #         "COMPONENTS": ["Elem", "Load", "Stage", "Step", "Force-I", "Force-J"],
+    #         "NODE_ELEMS": {"KEYS": [eles[0]]},
     #         "LOAD_CASE_NAMES": ["合计(CS)"],
     #         "OPT_CS": True,
-    #         "STAGE_STEP": ["成桥状态:002(最后)"],
     #     }
     # }
-    # MidasAPI("POST", "/POST/TABLE", get_json)
+    # MidasAPI("POST", "/POST/TABLE", POST_json)
+    with open(
+        "Output2.json",
+        "r",
+        encoding="gbk",
+        errors="ignore",
+    ) as f:
+        temp_json2 = f.read()
+    extracted_string = re.search(r"(\"TrussForce\":+)(.+)}", temp_json2).group(0)
+    temp_json2 = json.loads("{" + extracted_string)
+    temp_value2 = truss_force_tablejson_to_table(temp_json2)
+    step_name = temp_value2["Step"].iloc[-3]
+
+    # 格式化阶段步骤名称
+    ## 获取所有阶段的信息
+    allstage = MidasAPI("GET", "/db/STAG", {})
+    ## 获取最后一个阶段的信息
+    last_step_information = list(allstage["STAG"].items())[-1]
+    ## 获取最后一个阶段的名称
+    stagename = last_step_information[-1]["NAME"]
+    STAGE_STEP = f"{stagename}:{step_name}"
+    print(STAGE_STEP)
